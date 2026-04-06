329d17234e075fc1bc8040e7266f77d0a6afbd31
Sanjay Margabandu <Sanjaympro113@gmail.com>
2026-04-06 18:47:33 -0500

    Fix MongoDB

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,18 @@
 import os
 from dotenv import load_dotenv
-
+import certifi
+from pymongo import MongoClient
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://sanjaym13_db:Universe113_@softwarelabgroup6.fwowcy3.mongodb.net/?appName=SoftwareLabGroup6")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret-key-change-in-production")
 HWSET1_CAPACITY = int(os.getenv("HWSET1_CAPACITY", "100"))
 HWSET2_CAPACITY = int(os.getenv("HWSET2_CAPACITY", "100"))
+
+client = MongoClient(
+    MONGODB_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
+db = client.get_default_database()
