0f32885dc84a0ac33786b1348cb29153fe50c080
Sanjay Margabandu <Sanjaympro113@gmail.com>
2026-04-06 19:16:34 -0500

    db fix2

--- backend/config.py
+++ backend/config.py
@@ -4,15 +4,13 @@ import certifi
 from pymongo import MongoClient
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://sanjaym13_db:Universe113_@softwarelabgroup6.fwowcy3.mongodb.net/HardwareSetG6?appName=SoftwareLabGroup6")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://sanjaym13_db:Universe113_@softwarelabgroup6.fwowcy3.mongodb.net/HardwareSetG6?tls=true&tlsAllowInvalidCertificates=false")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret-key-change-in-production")
 HWSET1_CAPACITY = int(os.getenv("HWSET1_CAPACITY", "100"))
 HWSET2_CAPACITY = int(os.getenv("HWSET2_CAPACITY", "100"))
 
 client = MongoClient(
-    MONGODB_URI,
-    tls=True,
-    tlsCAFile=certifi.where()
+    MONGODB_URI
 )
 
 db = client.get_default_database()
